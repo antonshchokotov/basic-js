4339e1f3b83c4219fefe5684c50e20aa6c763462
Anton Shchokotov <58229945+antonshchokotov@users.noreply.github.com>
2020-02-24 19:47:16 +0200

    feat: add solution for extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,11 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+module.exports = function repeater(string, options) {
+  const addition = Array(options.additionRepeatTimes || 1)
+            .fill(options.addition !== undefined
+                    ? String(options.addition)
+                    : '')
+            .join((options.additionSeparator || '|')) || '';
+  const result = Array(options.repeatTimes || 1)
+            .fill(String(string)+addition)
+            .join(options.separator || '+');
+  return result;
+}
