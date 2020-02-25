d7e1fb7e0213814f3609200d0dbaa20fcbb4f8f3
Anton Shchokotov <58229945+antonshchokotov@users.noreply.github.com>
2020-02-25 09:48:21 +0200

    feat: add solution for simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,29 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+  addLink(value = '') {
+    this.chain.push(value === '' ? `( )` : `( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (0 < position && position <= this.chain.length) {
+      this.chain.splice(position - 1, 1);
+    } else {
+      this.chain = [];
+      throw new Error('cannot remove link');
+    }
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    const chain = [...this.chain].join('~~');
+    this.chain = [];
+    return chain;
   }
 };
 
