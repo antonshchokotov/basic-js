42fe70562185b299d7b4ea3cdb25931e4c98dce2
Anton Shchokotov <58229945+antonshchokotov@users.noreply.github.com>
2020-02-26 09:30:42 +0200

    feat: add solution for vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,52 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(flag = true) {
+        this.direct = flag;
+    }
+    
+    encrypt(message, key) {
+        if (!message || !key) {
+            throw new Error();
+        }
+        const M = [...message];
+        const K = [...key];
+        while (K.length < M.length) {
+            K.push(...K);
+        }
+        const C = [];
+        for (let l of M) {
+            if (/[a-z]/i.test(l)) {
+                let letter = l.toUpperCase().charCodeAt() - 65;
+                let key = K.shift().toUpperCase().charCodeAt() - 65;
+                C.push( String.fromCharCode( ( ( letter + key ) % 26 ) + 65 ) );
+            } else {
+                C.push(l);
+            }
+        }
+        return this.direct ? C.join('') : C.reverse().join('');
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key) {
+            throw new Error();
+        }
+        const C = [...encryptedMessage];
+        const K = [...key];
+        while (K.length < C.length) {
+            K.push(...K);
+        }
+        const M = [];
+        for (let l of C) {
+            if (/[a-z]/i.test(l)) {
+                let encryptedLetter = l.toUpperCase().charCodeAt() - 65;
+                let key = K.shift().toUpperCase().charCodeAt() - 65;
+                let letter = (encryptedLetter - key) % 26;
+                if(letter < 0) letter += 26;
+                M.push( String.fromCharCode( letter + 65 ) );
+            } else {
+                M.push(l);
+            }
+        }
+        return this.direct ? M.join('') : M.reverse().join('');
     }
 }
 
